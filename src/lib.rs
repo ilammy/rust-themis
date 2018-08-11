2f009673f0ec297a1af28cdb3c600786f38dc6b4
ilammy <a.lozovsky@gmail.com>
2018-08-11 22:18:23 +0300

    Do not mention explicit names in copyrights
    Having a specific list of names in copyrights gets problematic when
    other people start contributing to the project or effective project
    ownership changes. Instead of using names just refer to "$package
    developers". Exact authorship of each and every line can be obtained
    from git history.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 ilammy
+// Copyright 2018 (c) rust-themis developers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
