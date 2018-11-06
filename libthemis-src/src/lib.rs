6953f27acde02e920569378aea0420e9a3eb4c55
ilammy <a.lozovsky@gmail.com>
2018-11-06 22:14:12 +0200

    Use vendored BoringSSL in libthemis-src
    Actually, do not let the user configure engine at all. If they
    want a custom build of Themis they can do it themselves and tell
    libthemis-sys to use it. Vendored build of rust-themis should
    'just work' without any 3rd-party dependencies in the system.
    That's why we should use the vendored BoringSSL version which
    is used for Themis on Android.
    
    Using Rust for build scripts is SO MUCH more verbose than shell.
    Well, whatever... Also, BoringSSL uses CMake for its build
    system. And Go.
    
    It would probably be better to use `cmake` crate for invoking
    CMake. However, I tried and it does not really work well with
    the tests for `libthemis-src`. So maybe later.

--- libthemis-src/src/lib.rs
+++ libthemis-src/src/lib.rs
@@ -22,6 +22,7 @@ extern crate tempfile;
 use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 /// A builder (literally!) for Themis, produces [`Artifacts`].
 ///
@@ -57,10 +58,13 @@ impl Build {
 
     /// Builds Themis, panics on any errors.
     pub fn build(&self) -> Artifacts {
-        let src_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("themis");
         let out_dir = self.out_dir.as_ref().expect("OUT_DIR not set");
-        let build_dir = out_dir.join("build");
-        let install_dir = out_dir.join("install");
+        let themis_src_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("themis");
+        let themis_build_dir = out_dir.join("build");
+        let themis_install_dir = out_dir.join("install");
+        let ssl_src_dir = themis_src_dir.join("third_party/boringssl/src");
+        let ssl_build_dir = out_dir.join("boringssl-build");
+        let ssl_install_dir = out_dir.join("boringssl-install");
 
         // Themis uses in-source build. Cargo requires build scripts to never write anything
         // outside of OUT_DIR so we just have to copy the source code there.
@@ -68,32 +72,95 @@ impl Build {
         if !out_dir.exists() {
             fs::create_dir(&out_dir).expect("mkdir themis");
         }
-        if build_dir.exists() {
-            fs::remove_dir_all(&build_dir).expect("rm -r themis/build");
+        if themis_build_dir.exists() {
+            fs::remove_dir_all(&themis_build_dir).expect("rm -r themis/build");
         }
-        if install_dir.exists() {
-            fs::remove_dir_all(&install_dir).expect("rm -r themis/install");
+        if themis_install_dir.exists() {
+            fs::remove_dir_all(&themis_install_dir).expect("rm -r themis/install");
+        }
+        if ssl_build_dir.exists() {
+            fs::remove_dir_all(&ssl_build_dir).expect("rm -r boringssl/build");
+        }
+        if ssl_install_dir.exists() {
+            fs::remove_dir_all(&ssl_install_dir).expect("rm -r boringssl/install");
         }
 
-        copy_dir::copy_dir(&src_dir, &build_dir).expect("cp -r src build");
-        fs::create_dir(&install_dir).expect("mkdir themis/install");
+        copy_dir::copy_dir(&themis_src_dir, &themis_build_dir).expect("cp -r src build");
+        fs::create_dir(&themis_install_dir).expect("mkdir themis/install");
+        fs::create_dir(&ssl_build_dir).expect("mkdir boringssl/build");
+        fs::create_dir(&ssl_install_dir).expect("mkdir boringssl/install");
 
-        let mut make = make_cmd::make();
+        // First we have to build vendored BoringSSL which will act as cryptographic engine
+        // for Themis. There is no choice of the backend for the user. If you want a custom
+        // build then do it yourself and point libthemis-sys to the resulting artifacts.
+        // This crate produces Themis binary that depends only on the system C library.
+        // BoringSSL uses CMake for configuration and Make for build.
 
-        make.args(&["-C".as_ref(), build_dir.as_os_str()])
-            .env("PREFIX", &install_dir)
+        let build_type = if cfg!(debug) { "Debug" } else { "Release" };
+        let mut boringssl_configure = Command::new("cmake");
+        boringssl_configure
+            .current_dir(&ssl_build_dir)
+            .arg(format!("-DCMAKE_BUILD_TYPE={}", build_type))
+            .arg(&ssl_src_dir);
+        run(boringssl_configure, "BoringSSL configuration");
+
+        let mut boringssl_build = make_cmd::make();
+        boringssl_build
+            .current_dir(&ssl_build_dir)
+            .arg("crypto")
+            .arg("decrepit")
+            .arg("ssl");
+        run(boringssl_build, "BoringSSL build");
+
+        // It's so nice to have an "install" target available so that we don't have to figure out
+        // what the build artifacts are and copy them manually. Thank you, Google! Great usability!
+
+        copy_dir::copy_dir(ssl_src_dir.join("include"), ssl_install_dir.join("include"))
+            .expect("install boringssl/include");
+        fs::create_dir(ssl_install_dir.join("lib")).expect("mkdir boringssl/lib");
+        fs::copy(
+            ssl_build_dir.join("crypto/libcrypto.a"),
+            ssl_install_dir.join("lib/libcrypto.a"),
+        ).expect("install libcrypto.a");
+        fs::copy(
+            ssl_build_dir.join("decrepit/libdecrepit.a"),
+            ssl_install_dir.join("lib/libdecrepit.a"),
+        ).expect("install libdecrepit.a");
+        fs::copy(
+            ssl_build_dir.join("ssl/libssl.a"),
+            ssl_install_dir.join("lib/libssl.a"),
+        ).expect("install libssl.a");
+
+        // Finally we can build Themis. Note that we explicitly instruct the build
+        // to use our BoringSSL installation created on the previous step.
+
+        let mut themis_build_and_install = make_cmd::make();
+        themis_build_and_install
+            .current_dir(&themis_build_dir)
+            .env("PREFIX", &themis_install_dir)
+            .env("ENGINE", "boringssl")
+            .env("ENGINE_INCLUDE_PATH", ssl_install_dir.join("include"))
+            .env("ENGINE_LIB_PATH", ssl_install_dir.join("lib"))
             .arg("install");
-
-        let status = make.status().expect("make");
-        if !status.success() {
-            panic!("{:?} failed: exit={}", make, status);
+        if cfg!(debug) {
+            themis_build_and_install.env("DEBUG", "1");
         }
+        run(themis_build_and_install, "Themis build & install");
 
         Artifacts {
-            include_dir: install_dir.join("include"),
-            lib_dir: install_dir.join("lib"),
+            include_dir: themis_install_dir.join("include"),
+            lib_dir: themis_install_dir.join("lib"),
             libs: vec!["themis".to_owned(), "soter".to_owned()],
         }
+    }
+}
+
+fn run(mut command: Command, what: &str) {
+    let status = command
+        .status()
+        .expect(&format!("failed to execute {}", what));
+    if !status.success() {
+        panic!("{} failed: {}", what, status);
     }
 }
 
