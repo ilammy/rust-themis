b7e76940b4749e22e53e3fa8240882b701857c5d
ilammy <a.lozovsky@gmail.com>
2018-11-16 22:58:01 +0200

    Use pkg-config exclusively
    I've been trying to get static libraries working for two weeks and I've
    got enough of this crap. pkg-config is almost two decades old by now.
    Please be a good UNIX citizen and provide *.pc files for your libraries.
    If you're a special snowflake who has to build their own libraries then
    go and write some *.pc files yourself and set PKG_CONFIG_PATH to them.
    I don't want to maintain FIVE approaches for locating a single library
    (with only one of them -- pkg-config -- actually supporting static
    linkage in a good way).
    
    So drop almost everything from the build script of libthemis-sys and
    rely on pkg-config exclusively to find the library and print Cargo
    instructions for us. See how few lines of code it takes? That's why.
    We require "libthemis" and "libsoter" packages to be available.
    
    Why does Soter need to be linked explicitly in when Themis should
    depend on it and pull it in? That's because of "themis_shims" (see
    wrapper.c) use symbols from libsoter so we have to link against it.
    We can avoid this with static linkage everywhere and with dynamic
    linkage on Linux due to way they work, but dynamic linkage on macOS
    must be strict.
    
    Also, don't use Homebrew for guessing the location of the libraries.
    Normally it installs into /usr/local and the library will be available
    by default. In case of keg-only installations (like OpenSSL) you have
    to do special trickery anyway. Let the end user resolve these issues.
    They should set PKG_CONFIG_PATH appropriately.

--- libthemis-sys/build.rs
+++ libthemis-sys/build.rs
@@ -16,27 +16,18 @@ extern crate bindgen;
 extern crate cc;
 extern crate pkg_config;
 
-use std::collections::HashSet;
 use std::env;
-use std::ffi::OsString;
 use std::path::{Path, PathBuf};
-use std::process::Command;
-use std::str;
+
+use pkg_config::Library;
 
 fn main() {
-    let (include_dir, lib_dir, libs) = get_themis();
-    let linkage = select_linkage(&lib_dir, &libs);
-
-    println!("cargo:rustc-link-search=native={}", lib_dir.display());
-    println!("cargo:include={}", include_dir.display());
-    for lib in libs {
-        println!("cargo:rustc-link-lib={}={}", linkage, lib);
-    }
+    let themis = get_themis();
 
     let whitelist = "(THEMIS|themis|secure_(comparator|session)|STATE)_.*";
     let bindings = bindgen::Builder::default()
-        .clang_arg(format!("-I{}", include_dir.display()))
-        .clang_arg(format!("-L{}", lib_dir.display()))
+        .clang_args(clang_include_paths(&themis))
+        .clang_args(clang_library_paths(&themis))
         .header("src/wrapper.h")
         .whitelist_function(whitelist)
         .whitelist_type(whitelist)
@@ -53,23 +44,19 @@ fn main() {
     cc::Build::new()
         .file("src/wrapper.c")
         .include("src")
-        .include(&include_dir)
+        .includes(&themis.include_paths)
         .compile("themis_shims");
 }
 
-fn env_var(name: &str) -> Option<OsString> {
-    println!("cargo:rerun-if-env-changed={}", name);
-    env::var_os(name)
-}
-
-/// Embarks on an incredible adventure and returns with an include directory, library directory,
-/// and a list of Themis libraries.
-fn get_themis() -> (PathBuf, PathBuf, Vec<String>) {
-    None.or_else(|| probe_environment())
-        .or_else(|| probe_homebrew())
-        .or_else(|| probe_pkg_config())
-        .or_else(|| probe_standard_locations())
-        .expect(&format!(
+/// Embarks on an incredible adventure and returns with a suitable Themis (or dies trying).
+fn get_themis() -> Library {
+    let result = pkg_config::Config::new()
+        .env_metadata(true)
+        .arg("libsoter") // TODO: remove this together with themis_shims
+        .probe("libthemis");
+    match result {
+        Ok(library) => return library,
+        Err(error) => panic!(format!(
             "
 
 `libthemis-sys` could not find Themis installation in your system.
@@ -82,178 +69,50 @@ Please refer to the documentation for installation instructions:
 
     https://github.com/cossacklabs/themis#quickstart
 
-This crate can use `pkg-config` and `brew` to locate the library.
-You may help it by installing these tools and making sure that
-they are correctly configured.
+This crate uses `pkg-config` to locate the library. If you use
+non-standard installation of Themis then you can help pkg-config
+to locate your library by setting the PKG_CONFIG_PATH environment
+variable to path where `libthemis.pc` file is located.
 
-If you are sure that the library is installed in the system
-but this crate still fails to locate it then you can help it
-by setting the following environment variables: THEMIS_DIR,
-THEMIS_INCLUDE_DIR, THEMIS_LIB_DIR and trying again.
-
-"
-        ))
-}
-
-/// Checks environment overrides for Themis locations.
-fn probe_environment() -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    None.or_else(|| probe_environment_include_lib())
-        .or_else(|| probe_environment_install_dir())
-}
-
-fn probe_environment_include_lib() -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    let include_dir = env_var("THEMIS_INCLUDE_DIR");
-    let lib_dir = env_var("THEMIS_LIB_DIR");
-    if include_dir.is_some() && lib_dir.is_some() {
-        let include_dir = include_dir.unwrap();
-        let lib_dir = lib_dir.unwrap();
-        probe_location(&include_dir, &lib_dir)
-    } else {
-        None
+{}
+",
+            error
+        )),
     }
 }
 
-fn probe_environment_install_dir() -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    if let Some(install_dir) = env_var("THEMIS_DIR").map(|s| PathBuf::from(s)) {
-        probe_install_location(&install_dir)
-    } else {
-        None
-    }
+fn clang_include_paths(library: &Library) -> Vec<String> {
+    library
+        .include_paths
+        .iter()
+        .map(|p| format!("-I{}", p.display()))
+        .collect()
 }
 
-/// Tries asking Homebrew for directions if available.
-fn probe_homebrew() -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    fn prefix(formula: &str) -> Option<PathBuf> {
-        let output = Command::new("brew")
-            .arg("--prefix")
-            .arg(formula)
-            .env("HOMEBREW_NO_AUTO_UPDATE", "1")
-            .output();
+fn clang_library_paths(library: &Library) -> Vec<String> {
+    library
+        .link_paths
+        .iter()
+        .map(|p| format!("-L{}", p.display()))
+        .collect()
+}
 
-        if let Ok(output) = output {
-            if output.status.success() {
-                if let Ok(path) = str::from_utf8(&output.stdout) {
-                    return Some(PathBuf::from(path.trim()));
-                }
-            }
+trait CCBuildEx {
+    fn includes<I>(&mut self, dirs: I) -> &mut Self
+    where
+        I: IntoIterator,
+        I::Item: AsRef<Path>;
+}
+
+impl CCBuildEx for cc::Build {
+    fn includes<I>(&mut self, dirs: I) -> &mut Self
+    where
+        I: IntoIterator,
+        I::Item: AsRef<Path>,
+    {
+        for dir in dirs {
+            self.include(dir);
         }
-
-        None
-    }
-
-    None.or_else(|| prefix("themis").and_then(probe_install_location))
-        .or_else(|| prefix("themis-boringssl").and_then(probe_install_location))
-        .or_else(|| prefix("themis-openssl").and_then(probe_install_location))
-        .or_else(|| prefix("themis-libressl").and_then(probe_install_location))
-}
-
-/// Tries asking pkg-config for directions if available.
-fn probe_pkg_config() -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    pkg_config::Config::new()
-        .cargo_metadata(false)
-        .probe("themis")
-        .ok()
-        .and_then(|library| {
-            assert_eq!(library.include_paths.len(), 1);
-            assert_eq!(library.link_paths.len(), 1);
-            probe_location(&library.include_paths[0], &library.link_paths[0])
-        })
-}
-
-/// Makes a last-ditch effort with an educated guess and looks for Themis at standard locations.
-fn probe_standard_locations() -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    None.or_else(|| probe_install_location("/usr/local"))
-        .or_else(|| probe_install_location("/usr"))
-}
-
-fn probe_install_location<P: AsRef<Path>>(prefix: P) -> Option<(PathBuf, PathBuf, Vec<String>)> {
-    let prefix = prefix.as_ref();
-    let include_dir = prefix.join("include");
-    let lib_dir = prefix.join("lib");
-    probe_location(&include_dir, &lib_dir)
-}
-
-fn probe_location<I, L>(include_dir: I, lib_dir: L) -> Option<(PathBuf, PathBuf, Vec<String>)>
-where
-    I: AsRef<Path>,
-    L: AsRef<Path>,
-{
-    fn exists_in<P: AsRef<Path>, F: Fn(&Path) -> bool>(path: P, predicate: F) -> bool {
-        if let Ok(files) = path.as_ref().read_dir() {
-            files
-                .filter_map(|e| e.ok().map(|e| e.path()))
-                .any(|path| predicate(&path))
-        } else {
-            false
-        }
-    }
-
-    fn like_library(path: &Path, substr: &str) -> bool {
-        let prefix = format!("lib{}", substr);
-        path.file_name()
-            .and_then(|s| s.to_str())
-            .map_or(false, |name| name.starts_with(&prefix))
-    }
-
-    let include_dir = PathBuf::from(include_dir.as_ref());
-    let lib_dir = PathBuf::from(lib_dir.as_ref());
-    let libs = vec!["themis".to_owned(), "soter".to_owned()];
-
-    if !include_dir.join("themis/themis.h").exists() {
-        return None;
-    }
-    if !include_dir.join("soter/soter.h").exists() {
-        return None;
-    }
-    if !exists_in(&lib_dir, |f| like_library(f, "themis")) {
-        return None;
-    }
-    if !exists_in(&lib_dir, |f| like_library(f, "soter")) {
-        return None;
-    }
-
-    Some((include_dir, lib_dir, libs))
-}
-
-/// Decides whether we should link available libraries statically or dynamically.
-fn select_linkage(lib_dir: &PathBuf, libs: &Vec<String>) -> &'static str {
-    // First check for explicit instructions.
-    if let Some(linkage) = env_var("THEMIS_STATIC").and_then(|s| s.into_string().ok()) {
-        return if linkage == "0" { "dylib" } else { "static" };
-    }
-
-    // Now see what files we actually have available in the library directory
-    // which look like our libraries.
-    let files = lib_dir
-        .read_dir()
-        .expect(&format!("Themis library directory: {}", lib_dir.display()))
-        .filter_map(|e| e.ok().and_then(|e| e.file_name().into_string().ok()))
-        .filter(|filename| libs.iter().any(|lib| filename.contains(lib)))
-        .collect::<HashSet<_>>();
-
-    // Then check whether there is a full set of static or dynamic libraries available.
-    let can_static = libs.iter().all(|lib| {
-        let static_lib = format!("lib{}.a", lib);
-        files.contains(&static_lib)
-    });
-    let can_dylib = libs.iter().all(|lib| {
-        let dylib_macos = format!("lib{}.dylib", lib);
-        let dylib_linux = format!("lib{}.so", lib);
-        files.contains(&dylib_macos) || files.contains(&dylib_linux)
-    });
-
-    // And finally make a decision based on the intelligence we've gathered.
-    match (can_static, can_dylib) {
-        (true, false) => "static",
-        (false, true) => "dylib",
-
-        (false, false) => panic!(
-            "Themis library directory {} missing suitable libraries",
-            lib_dir.display()
-        ),
-
-        // If we have either static or dynamic libraries available then prefer dynamic linkage
-        // because this is a cryptographic library which could benefit from security upgrades.
-        (true, true) => "dylib",
+        self
     }
 }
