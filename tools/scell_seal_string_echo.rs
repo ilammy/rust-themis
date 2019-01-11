842aaacebb454aeb7cc8f8162387ce907df7c52d
ilammy <a.lozovsky@gmail.com>
2019-01-11 18:37:34 +0200

    update copyright years
    rrright, it is 2019 already

--- tools/scell_seal_string_echo.rs
+++ tools/scell_seal_string_echo.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 (c) rust-themis developers
+// Copyright 2019 (c) rust-themis developers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
